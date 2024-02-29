69a592d0ae3a75072abd05e4c2a99f3180607d68
ZhiNen <a1679415186@163.com>
2024-02-29 14:58:19 +0800

    sql语法

--- sql/members.sql
+++ sql/members.sql
@@ -6,17 +6,17 @@ MySQL - 8.0.26
 /*!40101 SET NAMES utf8 */;
 
 create table `mms_members` (
-	`id` char (108),
+	`id` char (36) not null primary key,
 	`created_at` timestamp ,
 	`updated_at` timestamp ,
 	`status` tinyint (3),
-	`username` varchar (765),
-	`password` varchar (765),
-	`nickname` varchar (765),
-	`mobile` varchar (765),
-	`email` varchar (765),
-	`avatar` varchar (1536),
-	`wechat_open_id` varchar (765),
+	`username` varchar (255),
+	`password` varchar (255),
+	`nickname` varchar (255),
+	`mobile` varchar (255),
+	`email` varchar (255),
+	`avatar` varchar (512),
+	`wechat_open_id` varchar (255),
 	`expired_at` timestamp ,
 	`rank_id` bigint (20)
 ); 
--- sql/reply.sql
+++ sql/reply.sql
@@ -6,12 +6,12 @@ MySQL - 8.0.26
 /*!40101 SET NAMES utf8 */;
 
 create table `mms_reply` (
-	`id` bigint (20),
+	`id` bigint (20) not null primary key auto_increment,
 	`created_at` timestamp ,
 	`updated_at` timestamp ,
-	`reply` varchar (6144),
-	`admin_id` varchar (192),
-	`admin_name` varchar (384),
+	`reply` varchar (2048),
+	`admin_id` varchar (64),
+	`admin_name` varchar (128),
 	`create_time` timestamp ,
 	`update_time` timestamp ,
 	`comment_id` bigint (20)
